35bbc5f066d9c6c19d30b38dd812fc47b7c789a5
Your Name <your-username@users.noreply.github.com>
2024-02-11 20:56:38 +0000

    Automated report

--- src/components/BuySellButton/index.jsx
+++ src/components/BuySellButton/index.jsx
@@ -3,7 +3,7 @@ import { FaDollarSign } from 'react-icons/fa';
 
 export default function Index({ width, height, currency }) {
   return (
-    <div className="buysell-container">
+    <div className='buysell-container'>
       <button
         style={{ width: width, height: height }}
         className='buysell-button'
